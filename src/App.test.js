35808b837def5bc9ea99d422592d6578f48b41c4
William S Chen <blackjamx5@hotmail.com>
2023-04-23 10:28:20 -0700

    added mocks

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { authMock } from 'tests/mocks/withAuthentication.test';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
