f99f215dc991f85d1b9915ddbb21221207a03c23
unknown <williamshengchen8@gmail.com>
2023-04-23 10:41:14 -0700

    added first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
-import { authMock } from 'tests/mocks/withAuthentication.test';
+// import App from './App';
+import DifficultyInput from 'components/post/draft/sub-components/difficulty-input';
+// import { authMock } from 'tests/mocks/withAuthentication.test';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(
+    <DifficultyInput
+      difficulty={"Hard"}
+      setDifficulty={() => console.log('Clicked')}
+      displayDifficulty={(val) => val}
+    />);
+  const linkElement = getByText(/Difficulty/i);
   expect(linkElement).toBeInTheDocument();
 });
